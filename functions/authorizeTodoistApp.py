79fe7f5995059f6d2882f986b78a865318bd14ab
Marcus Twichel <mtwichel@Marcuss-MacBook-Pro.local>
2018-09-13 23:42:35 -0600

    Added requirements file and authorizeTodoistApp

---
+++ functions/authorizeTodoistApp.py
@@ -0,0 +1,16 @@
+import requests
+import flask
+
+def authorizeTodoistApp(request):
+    code = request.args.get('code')
+    state = request.args.get('state')
+    data = {'client_id':'310591571acd4a20ac5616665445f52f',
+        'client_secret':'1611f1f32fed45bd87b4d6f0269f9de1',
+        'code':code}
+    
+    if state == 'yodarox314':
+        requests.post('https://todoist.com/oauth/access_token', data=data)
+        return 'All Good'
+    else:
+        #abandon ship
+        return abort(403)
